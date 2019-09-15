71476e26b55f193d0400d688ed390a3bd9e3e188
Jonah Colagross <colagjon000@battlegroundps.org>
2019-09-15 14:28:26 -0700

    Secondary init

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,7 +3,17 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		whoSwa_();
+		System.out.println("fly,\n");
+		ending();
+		
+	}
+	public static void whoSwa_() {
+		System.out.print("There was an old lady who swallowed a ");
+	}
+	public static void ending() {
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhaps she'll die.")
 	}
 
 }
